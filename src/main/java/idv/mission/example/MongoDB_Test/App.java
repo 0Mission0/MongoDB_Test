56a90bbaba88103fba7a60b042e59999094a414a
0Mission0 <0mission0@gmail.com>
2018-11-12 11:51:21 +0800

    MongoDB 練習

--- src/main/java/idv/mission/example/MongoDB_Test/App.java
+++ src/main/java/idv/mission/example/MongoDB_Test/App.java
@@ -1,13 +1,51 @@
 package idv.mission.example.MongoDB_Test;
 
+import java.util.Iterator;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+
 /**
- * Hello world!
- *
+ * RDBMS        MongoDB
+ * Database     Database
+ * Table        Collection
+ * Tuple/Row    Document
+ * column       Field
+ * Table Join   Embedded Documents
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        MongoClient client = new MongoClient("localhost", 27017);
+        MongoDatabase database = client.getDatabase("test");
+        database.getCollection("MissionTest");
+        MongoCollection<Document> collection = database.getCollection("MissionTest");
+
+        Document document = new Document();
+        document.append("User", "Mission");
+
+        // collection.insertOne(document);
+
+        MongoIterable<String> collectionNames = database.listCollectionNames();
+        Iterator<String> iterator = collectionNames.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("Collection: " + iterator.next());
+        }
+
+        FindIterable<Document> documents = collection.find();
+        Iterator<Document> iterator2 = documents.iterator();
+        while (iterator2.hasNext()) {
+            Document foundDocument = iterator2.next();
+            String user = (String) foundDocument.get("User");
+            System.out.println("Document: " + foundDocument);
+            System.out.println("User: " + user);
+        }
+
+        client.close();
+        System.out.println("End");
     }
 }
